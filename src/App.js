3b2c1e1b17638e1ac6429caa137f5f522247f2d9
rswee2uisedu <47045826+rswee2uisedu@users.noreply.github.com>
2019-02-28 19:38:53 -0500

    Issue 6 (#14)
    Closes #6 
    
    * #6 - Updated outdated npm packages.
    
    * #6 - initial pass at user feed data layout.
    
    * #6 - initial pass at feed data layout.
    
    * #6 - added FeedDataContext.
    
    * #6 - implemented withFeedData hoc, with first passing test and sample consumer component.
    
    * Added enzyme to test rendered components.
    
    * Added passing tests for withFeedData hoc.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import sampleUserFeeds from './services/sampleUserFeeds.json';
+import sampleFeedData from './services/sampleFeedData.json';
+import FeedDataContext from './services/FeedDataContext';
+import SampleConsumer from './services/SampleConsumer';
 
 class App extends Component {
+  state = {
+    feeds: {
+      userFeeds: sampleUserFeeds.userFeeds,
+      feedData: sampleFeedData.feedData
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.  Testing pipeline.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <FeedDataContext.Provider value={this.state.feeds}>
+        <SampleConsumer />
+      </FeedDataContext.Provider>
     );
   }
 }
---
+++ src/services/SampleConsumer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import withFeedData from './withFeedData';
+
+const SampleConsumer = props => {
+    const { feedData } = props;
+
+    return <div>
+        {`There are: ${feedData.userFeeds.length} feeds in the sample data set.`}
+    </div>
+};
+
+export default withFeedData(SampleConsumer);
