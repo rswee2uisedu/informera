7c581f31c223aa926adbc9718baa5a1e00e59c65
Jason King <kingjason222@gmail.com>
2019-03-05 20:03:41 -0500

    Issue 1 (#21)
    * rough implementation of feed item
    
    * issue #1 - implemented basic feed item component, added bootstrap for styling and dropdown menu
    
    * updated css to reflect changed filename
    
    * issue #1 removed package.json
    
    * re-added client/package.json, removed package-lock.json

--- client/src/components/FeedItem.js
+++ client/src/components/FeedItem.js
@@ -1,8 +1,32 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const FeedItem = props => <Fragment>
-    <h1>{props.feedData.title}</h1>
-    <div>{props.feedData.content}</div>
-</Fragment>
+
+const FeedItemOptions = props => {
+	return (
+		<Dropdown>
+			<Dropdown.Toggle variant="secondary" id="dropdown-basic">
+		    	<span className="hamburger"></span>
+			</Dropdown.Toggle>
+
+			<Dropdown.Menu>
+		    	<Dropdown.Item href="#/action-1">Option </Dropdown.Item>
+		    	<Dropdown.Item href="#/action-2">Option 2</Dropdown.Item>
+		    	<Dropdown.Item href="#/action-3">Option 3</Dropdown.Item>
+			</Dropdown.Menu>
+		</Dropdown>
+	)
+}
+
+
+const FeedItem = props => <div className="componentContainer">
+    <div className="feedItemHeading">
+    	<div className="feeditemTitle"><h3><a target="_blank" href={props.feedData.link}>{props.feedData.title}</a></h3></div>
+    	<div className="feedItemOptions"><FeedItemOptions /></div>
+    </div>
+    <div className="feedItemPreview"><p>{props.feedData.contentSnippet}</p></div>
+    <div className="feedItemImage">photo would be here<img src='' alt="image"/></div>
+</div>
 
 export default FeedItem;
