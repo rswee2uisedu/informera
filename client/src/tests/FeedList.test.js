88f248262f4f07f3cedb8870cb3c2c8543503a7d
rswee2uisedu <47045826+rswee2uisedu@users.noreply.github.com>
2019-03-17 19:53:08 -0400

    Testing snippet (#29)
    * Added snippet and base test for feed list.
    
    * Added readme.
    
    * Pulled snippet from global config.
    
    * Fixed typo.

---
+++ client/src/tests/FeedList.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import FeedList from '../components/FeedList';
+
+configure({ adapter: new Adapter });
+
+describe('FeedList tests.', () => {
+    test('Can successfully mount component.', () => {
+        mount(<FeedList />);
+    });
+});
