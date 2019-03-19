ac5f0002d04ff0e66f90e74ba8861dbe5d1cc327
thuffsmith <huffsmith07@gmail.com>
2019-03-18 20:55:37 -0400

    Options stored object overwriting new options object fixed. (#31)

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,15 @@ import SuggestedFeedsModal from './components/SuggestedFeedsModal';
 import LocalStorage from 'local-storage';
 
 const App = () => {
-  const [options, setOptions] = useState(LocalStorage.get('storedOptions') || DefaultOptions);
   const [suggestedFeedsModalVisible, setSuggestedFeedsModalVisible] = useState(true);
+
+  // Get users stored options
+  const storedOptions = LocalStorage.get('storedOptions');
+  let userOptions = DefaultOptions;
+  Object.keys(userOptions).map((key) => {
+    if (storedOptions[key]) userOptions[key] = storedOptions[key];
+  });
+  const [options, setOptions] = useState(userOptions);
 
   const toggleBooleanOption = (option) => {
     let newOptions = Object.assign({}, options);
